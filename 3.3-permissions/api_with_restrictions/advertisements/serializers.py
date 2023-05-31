1b8b162deebbe046adac049fb780f51a053b8196
Darya _V_13 <118071419+VoronovaDA@users.noreply.github.com>
2023-05-31 08:34:33 +0300

    Update serializers.py

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -15,10 +15,6 @@ class UserSerializer(serializers.ModelSerializer):
                   'last_name',)
 
 
-
-
-
-
 class AdvertisementSerializer(serializers.ModelSerializer):
     """Serializer для объявления."""
 
@@ -34,19 +30,12 @@ class AdvertisementSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Метод для создания"""
 
-        # Простановка значения поля создатель по-умолчанию.
-        # Текущий пользователь является создателем объявления
-        # изменить или переопределить его через API нельзя.
-        # обратите внимание на `context` – он выставляется автоматически
-        # через методы ViewSet.
-        # само поле при этом объявляется как `read_only=True`
         validated_data["creator"] = self.context["request"].user
         return super().create(validated_data)
 
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
-        # TODO: добавьте требуемую валидацию
         user = self.context['request'].user
         count_open = len(user.advertisements.filter(status='OPEN'))
         get_status = data.get('status')
