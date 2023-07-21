11f576ca0bb21780baaefd46f63b034e442993a3
Darya _V_13 <118071419+VoronovaDA@users.noreply.github.com>
2023-07-21 17:25:42 +0300

    Update settings.py

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = [194.67.104.194]
+ALLOWED_HOSTS = ['194.67.104.194']
 
 # Application definition
 
