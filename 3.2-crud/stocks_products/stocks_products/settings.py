274a7397b45d5abfadf8b9dd0bd6649687ee75d7
Darya _V_13 <118071419+VoronovaDA@users.noreply.github.com>
2023-07-22 21:20:18 +0300

    Update settings.py

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nw^y+m^wmxza1asgk+)!ua2qx9)g+#v=6%76-9i8i(6eqiw94j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'True'
 
-ALLOWED_HOSTS = ['95.163.235.127']
+ALLOWED_HOSTS = ['95.163.235.127', '127.0.0.1']
 
 
 # Application definition
